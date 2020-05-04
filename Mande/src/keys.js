f63632a251f3d6371e21176a59bc967340303c21
MatteoZL <53958320+MatteoZL@users.noreply.github.com>
2020-05-04 01:09:12 -0500

    Update keys.js

--- Mande/src/keys.js
+++ Mande/src/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
     database: 
     {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'C0m3g4ll3t4s',
-        database: 'mande',
+        host: 'bfzgtz7jbjrahwqw6c1l-postgresql.services.clever-cloud.com',
+        user: 'ur3psyinvswrijhuuzsn',
+        password: 'qYmfmni5TfSueV8UaSck',
+        database: 'bfzgtz7jbjrahwqw6c1l',
         port: '5432'
     }
     
