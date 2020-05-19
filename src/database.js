59f0321e6b86dae3a578356c68d02ccae4d4cd86
Kentruri <53955778+Kentruri@users.noreply.github.com>
2020-05-18 23:32:06 -0500

    HerokuAPP

--- src/database.js
+++ src/database.js
@@ -2,18 +2,18 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     //BASE DE DATOS LOCAL
-    host: 'postgres',
+    /*host: 'postgres',
     user: 'postgres',
     password: '123',
     database: 'mande',
-    port: '5432'
+    port: '5432'*/
 
     //BASE DE DATOS CLEVER CLOUD
-    //host: 'b7dlzbcosixp4ritgqny-postgresql.services.clever-cloud.com',
-    //user: 'ulno8vkkxiz86dqlu6g3',
-    //password: 'N7FtfQfgxYDwgcBuTRZQ',
-    //database: 'b7dlzbcosixp4ritgqny',
-    //port: '5432'
+    host: 'b7dlzbcosixp4ritgqny-postgresql.services.clever-cloud.com',
+    user: 'ulno8vkkxiz86dqlu6g3',
+    password: 'N7FtfQfgxYDwgcBuTRZQ',
+    database: 'b7dlzbcosixp4ritgqny',
+    port: '5432'
 });
 
 module.exports = pool;
